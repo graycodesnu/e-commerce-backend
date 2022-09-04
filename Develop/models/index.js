460990556f19f349eb4ada1eaa20235354932b05
Grayson Harvey <graycodes.nu@gmail.com>
2022-09-04 18:32:44 -0500

    worked in index.js to create foreign key relationships.

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -4,14 +4,28 @@ const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
+// TODO: Go through the onDelete relationships. Use 'CASCADE'
+
 // Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: 'category_id',
 
+});
 // Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: 'category_id',
 
+}),
 // Products belongToMany Tags (through ProductTag)
+Category.belongsToMany(Tag, {
+  foreignKey: 'category_id',
 
+}),
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  foreignKey: 'category_id',
 
+}),
 module.exports = {
   Product,
   Category,
